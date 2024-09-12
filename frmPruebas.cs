7cd4c9b41f6773cf26d4769a9b55306d9dd2da3a
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-12 15:34:03 -0500

    se codifico el algoritmo

--- frmPruebas.cs
+++ frmPruebas.cs
@@ -15,5 +15,18 @@ namespace Proyecto_Pruebas
         {
             InitializeComponent();
         }
+
+        int numero1, numero2, resultado;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            numero1 = Convert.ToInt32(textBox1.Text);
+            numero2 = Convert.ToInt32(textBox2.Text);
+
+            resultado = numero1 + numero2;
+
+            textBox3.Text = resultado.ToString();
+
+        }
     }
 }
